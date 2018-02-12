9747f98af63a53a965ffa63a36475ecb86f4c814
willian <willian.l.lucio@gmail.com>
2018-02-12 15:38:52 -0200

    capture errors of active records and action controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,8 +2,13 @@ module Api::V1
 
  class ApiController &lt; ApplicationController
 
-   # Métodos globais
+   rescue_from(ActiveRecord::RecordNotFound) do ||
+     render(json: {message: 'Not Found'}, status: :not_found) and return
+   end
+
+   rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+      render(json: {message: "Required parameter missing: #{parameter_missing_exception.param}"}, status: :bad_request) and return
+   end
 
  end
-
 end
